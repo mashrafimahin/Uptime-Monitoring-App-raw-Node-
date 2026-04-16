9068b5226af0a91532c027a7ab5ec52cd07d7f0d
Mahin <mashrafi.devs@gmail.com>
2026-04-16 22:37:21 +0600

    Secret Key Added for Crypto in Node

--- helpers/environments.js
+++ helpers/environments.js
@@ -12,11 +12,13 @@ const environments = {};
 environments.staging = {
   port: 3000,
   envName: "staging",
+  secretKey: "abcdabcdabcd",
 };
 
 environments.production = {
   port: 5000,
   envName: "production",
+  secretKey: "xyzxyzxyz",
 };
 
 // detetmine which environment was passed
